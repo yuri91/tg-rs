a438eb095d7a70baee39ea65b23ac5303643e9eb
Yuri Iozzelli <y.iozzelli@gmail.com>
2016-12-27 20:12:39 +0100

    Reorganized types. Added timeout. Added sendMessage

--- src/lib.rs
+++ src/lib.rs
@@ -15,24 +15,39 @@ use hyper::mime::{Mime, TopLevel, SubLevel, Attr, Value};
 
 use std::io::Read;
 
+use std::time::Duration;
+
 pub mod errors;
 use self::errors::*;
 
 pub mod types;
+use types::{request,response};
 
 pub struct Bot {
     base: String,
     client: Client,
-    offset: i64
+    offset: u64,
+    timeout: Duration
 }
+#[derive(Clone,Debug,Deserialize)]
+struct Response {
+    ok: bool,
+    result: Option<serde_json::Value>,
+    error_code: Option<u16>,
+    description: Option<String>
+}
+
 
 impl Bot {
     pub fn new(token: &str) -> Bot {
-        Bot {
+        let mut bot = Bot {
             base: format!("https://api.telegram.org/bot{}/", token),
             client: Client::new(),
-            offset: 0
-        }
+            offset: 0,
+            timeout: Duration::from_secs(120)
+        };
+        bot.client.set_read_timeout(Some(bot.timeout));
+        bot
     }
     fn send(&mut self, method: &str, body: &str) -> Result<serde_json::Value> {
         let mut res = self.client
@@ -45,40 +60,45 @@ impl Bot {
             .chain_err(|| format!("request for `{}` failed",method))?;
         let mut s = String::new();
         res.read_to_string(&mut s).chain_err(|| "cannot parse response")?;
-        println!("{:?}",&s);
-        let serialized : types::Response = serde_json::from_str(&s).chain_err(|| "cannot parse api response")?;
+        let serialized : Response = serde_json::from_str(&s).chain_err(|| "cannot parse api response")?;
         match serialized.ok {
             true => Ok(serialized.result.unwrap()),
             false => bail!(ErrorKind::ApiError(serialized.description.unwrap()))
         }
     }
-    pub fn get_chat(&mut self, chat_id: &str) -> Result<types::Chat> {
-        #[derive(Serialize)]
-        struct ChatId<'a> {
-            chat_id: &'a str
-        };
-        let body = ChatId { chat_id : chat_id};
+    pub fn get_chat(&mut self, chat_id: u64) -> Result<response::Chat> {
+        let body = request::Chat { chat_id : chat_id};
         let deserialized = serde_json::to_string(&body).unwrap();
         let resp = self.send("getChat",&deserialized)?;
         let serialized = serde_json::from_value(resp).chain_err(|| "cannot parse response of `getChat`")?;
         Ok(serialized)
     }
-    pub fn get_me(&mut self) -> Result<types::User> {
+    pub fn get_me(&mut self) -> Result<response::User> {
         let resp = self.send("getMe","")?;
         let serialized = serde_json::from_value(resp).chain_err(|| "cannot parse response of `getMe`")?;
         Ok(serialized)
     }
-    pub fn get_updates(&mut self) -> Result<Vec<types::Update>> {
-        #[derive(Serialize)]
-        struct GetUpdates {
-            offset: i64,
-        };
-        let body = GetUpdates { offset : self.offset};
+    pub fn get_updates(&mut self, timeout: u32) -> Result<Vec<response::Update>> {
+        let body = request::Update { offset: self.offset, timeout: std::cmp::min(100,timeout) as i32};
         let deserialized = serde_json::to_string(&body).unwrap();
         let resp = self.send("getUpdates",&deserialized)?;
-        let serialized: Vec<types::Update> = serde_json::from_value(resp)
+        let mut serialized: Vec<response::Update> = serde_json::from_value(resp)
             .chain_err(|| "cannot parse response of `getUpdates`")?;
-        self.offset = serialized.iter().fold(self.offset,|m,u| std::cmp::max(m,u.update_id)) + 1;
+        let new_offset = serialized.iter().fold(self.offset,|m,u| std::cmp::max(m,u.update_id)) + 1;
+        // Telegram sometimes sends already acknowleged messages
+        // A workaround seems to be to reset the offset whenever there are no
+        // new messages in the update
+        serialized.retain(|u| u.update_id >= self.offset);
+        self.offset = new_offset;
+        if serialized.len()==0 {
+            self.offset=0;
+        }
+        Ok(serialized)
+    }
+    pub fn send_message(&mut self, msg: &request::Message) -> Result<response::Message> {
+        let deserialized = serde_json::to_string(msg).unwrap();
+        let resp = self.send("sendMessage",&deserialized)?;
+        let serialized = serde_json::from_value(resp).chain_err(|| "cannot parse response of `sendMessage`")?;
         Ok(serialized)
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,56 +1,76 @@
-use serde_json::Value;
-
-#[derive(Clone,Debug,Deserialize)]
-pub struct Response {
-    pub ok: bool,
-    pub result: Option<Value>,
-    pub error_code: Option<u16>,
-    pub description: Option<String>
+pub mod request {
+    #[derive(Clone, Copy, Debug, Serialize)]
+    pub struct Update {
+        pub offset: u64,
+        pub timeout: i32
+    }
+    #[derive(Clone, Copy, Debug, Serialize)]
+    pub struct Chat {
+        pub chat_id: u64
+    }
+    #[derive(Clone, Copy, Debug, Serialize)]
+    pub enum ParseMode {
+        Markdown,
+        HTML
+    }
+    #[derive(Clone, Debug, Serialize, Default)]
+    pub struct Message<'a> {
+        pub chat_id: u64,
+        pub text: &'a str,
+        pub disable_web_preview: bool,
+        pub disable_notification: bool,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub parse_mode: Option<ParseMode>,
+        pub reply_to_message_id: Option<u64>,
+        // reply_markup: ?
+    }
 }
+pub mod response {
+    #[derive(Clone,Debug,Deserialize)]
+    pub struct User {
+        pub id: u64,
+        pub first_name: String,
+        pub last_name: Option<String>,
+        pub username: Option<String>,
+    }
 
-#[derive(Clone,Debug,Deserialize)]
-pub struct User {
-    pub id: u64,
-    pub first_name: String,
-    pub last_name: Option<String>,
-    pub username: Option<String>
-}
+    #[derive(Clone,Debug,Deserialize)]
+    pub enum ChatKind {
+        #[serde(rename = "private")]
+        Private,
+        #[serde(rename = "group")]
+        Group,
+        #[serde(rename = "supergroup")]
+        Supergroup,
+        #[serde(rename = "channel")]
+        Channel
+    }
 
-#[derive(Clone,Debug,Deserialize)]
-pub enum ChatKind {
-    #[serde(rename = "private")]
-    Private,
-    #[serde(rename = "group")]
-    Group,
-    #[serde(rename = "supergroup")]
-    Supergroup,
-    #[serde(rename = "channel")]
-    Channel
-}
+    #[derive(Clone,Debug,Deserialize)]
+    pub struct Chat {
+        pub id: u64,
+        #[serde(rename = "type")]
+        pub chat_type: ChatKind,
+        pub title: Option<String>,
+        pub first_name: Option<String>,
+        pub last_name: Option<String>,
+        pub username: Option<String>,
+        pub all_members_are_administrators: Option<bool>
+    }
 
-#[derive(Clone,Debug,Deserialize)]
-pub struct Chat {
-    pub id: u64,
-    #[serde(rename = "type")]
-    pub chat_type: ChatKind,
-    pub title: Option<String>,
-    pub first_name: Option<String>,
-    pub last_name: Option<String>,
-    pub username: Option<String>,
-    pub all_members_are_administrators: Option<bool>
-}
+    // TODO: complete this
+    #[derive(Clone,Debug,Deserialize)]
+    pub struct Message {
+        pub message_id: u64,
+        pub from: Option<User>,
+        pub date: u64,
+        pub chat: Chat,
+        pub text: Option<String>
+    }
 
-// TODO: complete this
-#[derive(Clone,Debug,Deserialize)]
-pub struct Message {
-    pub message_id: u64,
-    pub from: Option<User>,
-    pub date: u64,
-    pub chat: Chat
-}
-
-#[derive(Clone,Debug,Deserialize)]
-pub struct Update {
-    pub update_id: i64,
-    pub message: Option<Message>
+    #[derive(Clone,Debug,Deserialize)]
+    pub struct Update {
+        pub update_id: u64,
+        pub message: Option<Message>
+    }
 }
